f2242415529d52d03379355a1952dd6594f58bdd
Rakshith <rakshithlokesha@gmail.com>
2023-04-16 23:54:22 +0530

    some info in comments

--- src/main/java/com/example/rediscache/RedisCacheApplication.java
+++ src/main/java/com/example/rediscache/RedisCacheApplication.java
@@ -13,6 +13,17 @@ public class RedisCacheApplication {
 //            <groupId>org.springframework.boot</groupId>
 //            <artifactId>spring-boot-starter-data-redis</artifactId>
 //        </dependency>
+
+
+    //Yes, Jedis can be incorporated into the RedisTemplate provided by Spring Data Redis.
+    // RedisTemplate is a high-level abstraction for working with Redis,
+    // and it simplifies the interactions with the Redis server.
+    // Here's an example of how to configure RedisTemplate with Jedis:
+
+
+    //In a Spring Boot application, using RedisTemplate is recommended over the direct use of Jedis.
+    // RedisTemplate is a high-level abstraction provided by Spring Data Redis that simplifies
+    // the interaction with Redis server and offers additional features.
     public static void main(String[] args) {
         SpringApplication.run(RedisCacheApplication.class, args);
     }
